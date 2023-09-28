45b342b4d80bc350ee8a999b785b8d0ce9dbcadc
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-09-28 13:38:21 +0100

    update

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -29,6 +29,7 @@ const Dashboard = () => {
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  console.log(data);
 
   return (
   <div className={styles.container}>
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,57 +2,39 @@ import React from 'react';
 import styles from './page.module.css'
 import Button from '../../../components/Button/Button';
 import Image from 'next/image';
+import { NOTFOUND } from 'dns';
+import { items }from './data'
+
+const getData = (cate) =>{
+  const data = items[cate];
+  if(data){
+    return data;
+  }
+  return NOTFOUND();
+}
 
 const Category = ({params}) => {
-  console.log(params)
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-              className={styles.img}
-              fill={true} 
-              src="/illustration.png" 
-              alt="" 
-            />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-              className={styles.img}
-              fill={true} 
-              src="/websites.jpg" 
-              alt="" 
-            />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-              className={styles.img}
-              fill={true} 
-              src="/apps.jpg" 
-              alt="" 
-            />
-        </div>
-      </div>
+      {data.map((item)=>(
+        <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.desc}</p>
+              <Button url="#" text="See More" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image 
+                  className={styles.img}
+                  fill={true} 
+                  src={item.image}
+                  alt="" 
+                />
+            </div>
+        </div>)
+        )}
     </div>
   )
 }
