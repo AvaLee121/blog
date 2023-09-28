7260628d97ec5d6d2560c566435567c3a14a658b
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-09-28 18:32:35 +0100

    update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
     <div className={styles.container}>
       <h1 className={styles.selectTitle}> Choose a gallery</h1>
       
-      <div className={styles.items} style={{color:"#bbb"}}>
+      <div className={styles.items} >
         <Link href="/portfolio/illustrations" className={styles.item} >
           <span className={styles.title}> Illustrations</span>
         </Link>
