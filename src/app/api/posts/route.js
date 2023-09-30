fabaf169ea6ea8bbc6e6296706fbd33992660329
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-09-30 20:48:21 +0100

    update

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -7,7 +7,7 @@ export const GET = async (request) => {
     try {
         await connect();
     
-        const posts = await Post.find(username && { username });
+        const posts = await Post.find();
     
         return new NextResponse(JSON.stringify(posts), { status: 200 });
       } catch (err) {
