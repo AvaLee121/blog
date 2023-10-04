1aefa340ab3533a0cbda238bb0b6e4f7fb45d9a8
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-10-04 14:28:16 +0100

    update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,13 @@
+"use client"
 import React from 'react';
 import styles from './page.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 async function getData() {
   const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store",
-    
   });
 
   if (!res.ok) {
@@ -14,7 +15,6 @@ async function getData() {
   }
 
   return res.json();
-  
 }
 
 const Blog = async () => {
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    const connection = await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+    return connection; // Return the connection instance for reuse
   } catch (error) {
+    console.error("MongoDB connection error:", error);
     throw new Error("Connection failed!");
-  }
-};
+  };
+}
 
 
 export default connect;
