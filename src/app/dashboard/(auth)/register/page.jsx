2d562c7a98bb9c7232e8134e2c36ee644705c539
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-10-05 16:41:34 +0100

    update

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -3,16 +3,19 @@
 import React, { useState } from 'react';
 import styles from './page.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const Register = () => {
 
   const [err, setErr] = useState(false);
 
+  const router = useRouter();
+
   const handleSubmit = async (e) =>{
     e.preventDefault();
-    const name = e.target(0).value;
-    const email = e.target(0).value;
-    const password = e.target(0).value;
+    const name = e.target[0].value;
+    const email = e.target[0].value;
+    const password = e.target[0].value;
   
     try {
       const res = await fetch("/api/auth/register", {
@@ -20,10 +23,16 @@ const Register = () => {
         headers: {
           "Content-Type":"application/json",
         }, 
-        
-      }
+        body: JSON.stringify({
+          name,
+          email,
+          password,
 
-      )
+        })
+      });
+
+      res.status === 201 && router.push("/dashboard/login?success=Account has been created");
+
     } catch (error) {
       setErr(true);
     }
