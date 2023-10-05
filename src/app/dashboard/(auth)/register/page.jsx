47ddb2882bfe300428f5f57edca302dce3fba9fc
Ava <55948841+AvaLee121@users.noreply.github.com>
2023-10-05 14:22:32 +0100

    update

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import Link from 'next/link';
 
 const Register = () => {
+
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const name = e.target(0).value;
+    const email = e.target(0).value;
+    const password = e.target(0).value;
+  
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json",
+        }, 
+        
+      }
+
+      )
+    } catch (error) {
+      setErr(true);
+    }
+
+
+  }
   return (
-    <div>
-      Register
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type='text' placeholder='username' className={styles.input} required/>
+        <input type='email' placeholder='email' className={styles.input} required/>
+        <input type='password' placeholder='password' className={styles.input} required/>
+        <button className={styles.button}>Register</button>
+      </form>
+      {err && "Something went wrong!"}
+      <Link href="/dashboard/login">Login with an existing account</Link>
     </div>
   )
 }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import useSWR from 'swr';
-
+import { useSession } from 'next-auth/react';
 const Dashboard = () => {
 
   /* const [data, setData] = useState([]);
@@ -26,9 +26,14 @@ const Dashboard = () => {
   }, []);
  */
 
+  const session = useSession();
+  
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+
   console.log(data);
 
   return (
