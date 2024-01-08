b95aa0ea374d15b855fb312254fce91ab2a2a523
Ava <55948841+AvaLee121@users.noreply.github.com>
2024-01-08 13:48:20 +0000

    update

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,8 @@
-"use client"
-
-import React, { useState } from 'react';
-import styles from './page.module.css';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
   const [error, setError] = useState(null);
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,57 +1,59 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import styles from './page.module.css';
-import useSWR from 'swr';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { Concert_One } from 'next/font/google';
-import Image from 'next/image';
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
 
-  /* const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  //OLD WAY TO FETCH DATA
 
-  useEffect(() => {
-    const getData = async () =>{
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
-          cache:"no-store"
-        });
-  
-    if (!res.ok) {
-      setErr(true);
-    }
-  
-    const data =  await res.json();
-    setData(data);
-    setIsLoading(false);
-    } 
-  }, []);
- */
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json()
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData()
+  // }, []);
 
   const session = useSession();
 
   const router = useRouter();
-
-  const fetcher = (...args) => fetch(...args).then(res => res.json());
+  
+  //NEW WAY TO FETCH DATA
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, mutate, error, isLoading } = useSWR(
     `/api/posts?username=${session?.data?.user.name}`,
     fetcher
-    );
+  );
 
-    console.log(data);
-
-  if(session.status === "loading"){
-    return <p>Loading...</p>
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
   }
-  if(session.status === "unauthenticated"){
-    router?.push("/dashboard/login")
-  }
- 
 
-  const handleSubmit = async (e) =>{
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const title = e.target[0].value;
     const desc = e.target[1].value;
@@ -69,61 +71,59 @@ const Dashboard = () => {
           username: session.data.user.name,
         }),
       });
-      mutate();
-      e.target.reset();
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
-
-  const handleDelete = async(id) =>{
+  const handleDelete = async (id) => {
     try {
-      await fetch(`api/posts/${id}`,{
+      await fetch(`/api/posts/${id}`, {
         method: "DELETE",
       });
       mutate();
-      
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      console.log(err);
     }
-  }
-  if(session.status === "authenticated"){
+  };
+
+  if (session.status === "authenticated") {
     return (
-     <div className={styles.container}>
-      <div className={styles.posts}>
-        {isLoading 
-        ? "loading"
-        : data?.map((post)=>(
-          <div className={styles.post} key={post._id}>        
-          <div className={styles.imgContainer}>
-            <Image src={post.img} alt="" width={200} height={100}/>
-          </div>
-          <h2 className={styles.postTilte}>{post.title}</h2>
-          <span className={styles.delete}
-          onClick={() => handleDelete(post._id)}
-          >X</span>
-      </div>
-      ))}
-      </div>
-      <form className={styles.new} onSubmit={handleSubmit}>
-        <h1>Add New Post</h1>
-        <input type="text" placeholder='Title'  className={styles.input}/>
-        <input type="text" placeholder='Desc'  className={styles.input}/>
-        <input type="text" placeholder='Image'  className={styles.input}/>
-        <textarea 
-          placeholder="Content" 
-          className={styles.textArea} 
-          cols="30" 
-          rows="10"
-        >     
-        </textarea>
-        <button className={styles.button}>Send</button>
-      </form>
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading"
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={100} />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
       </div>
     );
-  };
- 
+  }
 };
 
-export default Dashboard
+export default Dashboard;
